06d090a56b4eee20836cc1654300c07b3c0fdd57
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-30 00:37:59 +0200

    Basic Structure

---
+++ Source/MyResume.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyResume.Web.Startup))]
+namespace MyResume.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
