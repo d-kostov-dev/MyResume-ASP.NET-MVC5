c36a9c5baa73af00df6ea894eef2347265399317
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-30 21:58:40 +0200

    Added More Models

--- Source/MyResume.Models/Blog/Post.cs
+++ Source/MyResume.Models/Blog/Post.cs
@@ -43,11 +43,19 @@
             }
         }
 
+        [Required]
+        [StringLength(200, MinimumLength = 3)]
         public string Title { get; set; }
 
+        [Required]
+        [StringLength(2000, MinimumLength = 50)]
         public string Content { get; set; }
 
         public string Description { get; set; }
+
+        public string MetaKeywords { get; set; }
+
+        public string MetaDescription { get; set; }
 
         public int ImageId { get; set; }
 
---
+++ Source/MyResume.Models/SiteSetting.cs
@@ -0,0 +1,23 @@
+﻿namespace MyResume.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class SiteSetting : AuditInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        [Index(IsUnique = true)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 10)]
+        public string Description { get; set; }
+    }
+}
