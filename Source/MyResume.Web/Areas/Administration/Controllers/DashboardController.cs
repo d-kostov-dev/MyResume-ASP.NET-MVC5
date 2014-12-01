97ada7d6cf4a71a5f84e7f656385ae05b6a9efb6
d-kostov-dev <d.kostov.dev@gmail.com>
2014-12-01 17:58:19 +0200

    Dashboard Controller

--- Source/MyResume.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Source/MyResume.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace MyResume.Web.Areas.Administration.Controllers
+﻿namespace MyResume.Web.Areas.Administration.Controllers
 {
+    using System.Web.Mvc;
+
     public class DashboardController : AdminBaseController
     {
         public ActionResult Index()
